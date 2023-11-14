ae9269b9bed36bcc771f5f53ee9e9bbbb1bf01e9
Greg Arki <91910362+bubadev@users.noreply.github.com>
2023-11-14 15:00:22 +0300

    init

---
+++ ImageDownloader/DogDownloader/DogClient.cs
@@ -0,0 +1,60 @@
+﻿using System.Data;
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+
+namespace DogDownloader
+{
+    public class DogClient : IDisposable
+    {
+        HttpClient _client = new HttpClient();
+
+        public async Task<string> GetRandomDogImageUrlAsync()
+        {
+            var path = "https://dog.ceo/api/breeds/image/random";
+
+            HttpResponseMessage response = await _client.GetAsync(path);
+            response.EnsureSuccessStatusCode();
+            var responseModel = await response.Content.ReadFromJsonAsync<ResponseSingleMessageModel>();
+            return responseModel.Message;
+        }
+        public async Task<string[]> GetDogImageUrlByBreedAsync(string breed, int count = 1)
+        {
+            var path = $"https://dog.ceo/api/breed/{breed}/images/random/3";
+
+            HttpResponseMessage response = await _client.GetAsync(path);
+            response.EnsureSuccessStatusCode();
+            var responseModel = await response.Content.ReadFromJsonAsync<ResponseManyMessageModel>();
+            return responseModel.Message;
+        }
+
+        public static string GetBreedFromUrl(string url)
+        {
+            Regex regex = new Regex(@"https://images.dog.ceo/breeds/(\S+)/\S*");
+            MatchCollection matches = regex.Matches(url);
+            return matches.FirstOrDefault().Groups[1].Value;
+        }
+
+        #region Dispose
+        private bool disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _client.Dispose();
+                }
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ ImageDownloader/ImageDownloader/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+using DogDownloader;
+using static System.Net.Mime.MediaTypeNames;
+using System;
+using System.Diagnostics;
+
+namespace ImageDownloader
+{
+    internal class Program
+    {
+        static HttpClient client = new HttpClient(); 
+        static async Task Main(string[] args)
+        {
+            using(var dogClient  = new DogClient())
+            {
+                var url = await dogClient.GetRandomDogImageUrlAsync();
+
+                
+                var bytearray = await GetImageAsync(url);
+
+                // Путь прописан руками для скорости, не успевал причесать код
+                var filePath = "C:/Projects/ImageDownloader/ImageDownloader/Images/dog.jpg";
+                File.WriteAllBytes(filePath, bytearray);
+                Process.Start(new ProcessStartInfo()
+                {
+                    FileName = filePath,
+                    UseShellExecute = true
+                });
+
+                var breed = DogClient.GetBreedFromUrl(url);
+
+                var urls = await dogClient.GetDogImageUrlByBreedAsync(breed, 3);
+
+                for (int i = 0; i < urls.Length; i++)
+                {
+                    var imageBytes = await GetImageAsync(url);
+                    var imagePath = $"C:/Projects/ImageDownloader/ImageDownloader/Images/dog{i}.jpg";
+                    File.WriteAllBytes(imagePath, imageBytes);
+                    Process.Start(new ProcessStartInfo()
+                    {
+                        FileName = imagePath,
+                        UseShellExecute = true
+                    });
+                }
+            }
+            Console.ReadKey();
+        }
+        static async Task<byte[]> GetImageAsync(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+    }
+}
